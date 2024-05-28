aacbb430db93763b5b105708bb5fce87967d5952
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-05-28 21:53:54 +0900

    T6-12 Change Little Thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,7 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">Explore starter templates for Next.js.</p>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">Change Little Things</p>
         </a>
 
         <a
